8686f5935571d88e83aeece5ad8fa6af8e759508
UNSWmir <38741603+UNSWmir@users.noreply.github.com>
2018-05-03 22:37:43 +1000

    Update config.h

--- config.h
+++ config.h
@@ -1,14 +1,11 @@
-//TODO:  Change to generic names before making public
-
-
-const String    CHANNEL_ID    = "n5n09k7grwjf0zdc"; // The Readiness.io channel ID
-const String    SENSOR_ID     = "switch_generic"; // Your AgriWebb or made up sensor ID
-const String    TOPIC         = "water-level"; // The type of sensor or name of the data your sending
+const String    CHANNEL_ID    = "XXXXXX"; // The Readiness.io channel ID
+const String    SENSOR_ID     = "XXXXXX"; // Your AgriWebb or made up sensor ID
+const String    TOPIC         = "switch-generic"; // The type of sensor or name of the data your sending
 const String    VERSION       = "1";
 const String    FORMAT        = "";
 
-const char*     WIFI_SSID     = "FarmerNet"; // Your WiFi SSID / name
-const char*     WIFI_PASS     = "SwitchNode37294"; // Your WiFi password
+const char*     WIFI_SSID     = "XXXXXX"; // Your WiFi SSID / name
+const char*     WIFI_PASS     = "XXXXXX"; // Your WiFi password
 
 const uint8_t   TIMEZONE_OFFSET = 10; // The timezone the sensor is located in (eg. 10 for GMT)
 
